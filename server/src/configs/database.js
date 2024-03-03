f09e1a7a719094199c67e7db04b224d29c88ddfc
WraasTheOne <nicholas.mazur.hansen@gmail.com>
2024-03-03 12:40:46 +0100

    db added

--- server/src/configs/database.js
+++ server/src/configs/database.js
@@ -1,19 +1,25 @@
 // database.js
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
+  console.log('Connecting to MongoDB...');
   try {
-    // Use your MongoDB connection string
-    const connectionString = 'mongodb://localhost:27017/firstdb';
-    await mongoose.connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+
+    const connectionString = process.env.DB_CONNECTION_STRING;
+
+    const options = {
+      dbName: process.env.DB_NAME, 
+      user: process.env.DB_USERNAME,
+      pass: process.env.DB_PASSWORD,
+    };
+
+    await mongoose.connect(connectionString, options);
 
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
-    process.exit(1); // Exit the process with an error code
+    process.exit(1); 
   }
 };
 
