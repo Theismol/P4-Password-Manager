abbf3014beb3522b178fbb5478f047862c904383
Nierfeldt <oliver@nierfeldt.dk>
2024-03-04 14:14:17 +0100

    ready to skeleton

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,12 +1,31 @@
-/** @type {import('tailwindcss').Config}*/
+
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+
+// 1. Import the Skeleton plugin
+import { skeleton } from '@skeletonlabs/tw-plugin';
+
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+	// 2. Opt for dark mode to be handled via the class method
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		// 3. Append the path to the Skeleton package
+		join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		// 4. Append the Skeleton plugin (after other plugins)
+		skeleton({
+      themes: { preset: [ "wintry" ] }
+    })
+	]
+} satisfies Config;
 
-  theme: {
-    extend: {},
-  },
-
-  plugins: [],
-};
-
-module.exports = config;
+export default config;
+						
