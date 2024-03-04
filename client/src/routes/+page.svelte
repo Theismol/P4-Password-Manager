7a3e175c91f131e635bb5eb68cb12a6a0e3808b3
Nierfeldt <oliver@nierfeldt.dk>
2024-03-04 14:34:24 +0100

    login page

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script >
+import { focusTrap } from '@skeletonlabs/skeleton';
+
+</script>
+  
+
+
+<main>
+    <form use:focusTrap={true}>
+        
+        <input data-focusindex="1" type="text" placeholder="Username" />
+        <!-- Email will be the first focused -->
+        <input data-focusindex="0" type="password" placeholder="Password" />
+        <button class="btn variant-filled-primary">Submit</button>
+    </form>
+</main>
+
+
+<style>
+    /* Custom styles */
+    header {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    main {
+        margin-bottom: 20px;
+        text-align: center;
+    }
+
+    footer {
+        text-align: center;
+        font-size: 0.8em;
+    }
+</style>
