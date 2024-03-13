01b3012d0ed7bf700f96a6025add0ca29d3dd593
WraasTheOne <nicholas.mazur.hansen@gmail.com>
2024-03-13 08:20:09 +0100

    nu sendes cookies

--- client/src/routes/loginpage/loginLogic.ts
+++ client/src/routes/loginpage/loginLogic.ts
@@ -14,12 +14,23 @@ export const handleLogin = (e: SubmitEvent) => {
 const fetchData = async (username: string, password: string) => {
     try {
         const response = await axios.post(
-            'http://localhost:4000/login',
+            'http://localhost:4000/api/auth/login',
             {
                 username: username,
                 password: password
             }
-        , {headers: {'Content-Type': 'application/json'}});
+        , {headers: {'Content-Type': 'application/json'}, withCredentials: true});
+
+        const receivedCookies = response.headers['Set-Cookie'];
+
+        console.log("receivedCookies is ", receivedCookies)
+        
+      
+        if (receivedCookies) {
+            receivedCookies.forEach((cookie: string) => {
+              document.cookie = cookie; // Set received cookies
+            });
+         }
 
         console.log(response);
         } catch (error) {
