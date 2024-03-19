1ed80b4e1277bed4e2873dd78334cdee9e68b412
Oliver Nierfeldt <oliver@nierfeldt.dk>
2024-03-19 20:11:30 +0100

    fixed different sizes in boxes

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -7,13 +7,13 @@
 		<div class="m-4 col-span-3">
 			<h1 class="h1">Log in</h1>
 		</div>
-		<div class="col-span-3 w-full">
+		<div class="col-span-3 ">
 			<label class="h4" for="username">Username</label>
-			<input class="input" id="username" type="username" name="username" required />
+			<input class="input w-full" id="username" type="text" name="username" required />
 		</div>
-		<div class="col-span-3">
+		<div class="col-span-3 ">
 			<label class="h4" for="username">Password</label>
-			<input class="input" id="password" type="password" name="password" required />
+			<input class="input w-full" id="password" type="password" name="password" required />
 		</div>
         <div class="my-4 col-span-3">
             <button class="btn variant-filled-primary w-1/6 btn bg-gradient-to-br variant-gradient-tertiary-secondary" type="submit">Log in</button>
