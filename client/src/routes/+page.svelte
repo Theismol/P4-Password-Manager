5c0631f7de87e73b0ada5be611d067a0dd66711e
diana1420 <QY35AM@student.aau.dk>
2024-03-26 15:11:33 +0100

    deletedComment

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -3,22 +3,6 @@
 	import { goto } from "$app/navigation";
 
 </script>
-<!-- <script lang="ts">
-
-</script>
-
-
-
- <div class="container h-screen mx-auto flex justify-center items-center  ">
-	<div class="grid grid-cols-3 place-content-center">
-		<h1 class="h1 col-span-3">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-	</div>
-	<div>
-	<button class="btn variant-filled-primary">get started</button>
-	</div>
-</div> -->
-
 
   <div class="container flex flex-col items-center h-screen my-8">
 	<h1 class="text-4xl font-bold mb-4">This is the headline</h1>
