f83323bd51d3626745dc6a04f7b230b2843818e5
Theismol <theis.moldrup@gmail.com>
2024-03-26 17:39:16 +0100

    menubar only on main page now

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
-	import Icon from '@iconify/svelte'
-	import { goto } from '$app/navigation';
+
 
 	// Highlight JS
 	/*import hljs from 'highlight.js/lib/core';
@@ -24,24 +22,5 @@
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });*/
 </script>
-<AppShell scrollbarGutter="auto">
-	<svelte:fragment slot="header">
-		<AppBar  shadow="shadow-2xl" gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
-			<svelte:fragment slot="lead">
-				<div>
-					<button class="btn variant-filled-secondary mx-4" on:click={() => goto("/loginpage")}>Log in</button>
-					<button class="btn variant-filled-secondary" on:click={() => goto("/signup")}>Sign up</button>
-				</div>
-			</svelte:fragment>
-			<a href="/">
-				<h2 class="h2">Placeholder Name</h2>
-			</a>
-			<svelte:fragment slot="trail">
-				<a href="https://github.com/Theismol/P4-Password-Manager">
-					<Icon icon="mdi:github" height="auto" />
-				</a>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<slot />
-</AppShell>
+
+<slot />
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,24 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
+<script lang="ts">
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+	import Icon from '@iconify/svelte'
+	import { goto } from '$app/navigation';
+</script>
+<AppBar  shadow="shadow-2xl" gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">
+		<div>
+			<button class="btn variant-filled-secondary mx-4" on:click={() => goto("/loginpage")}>Log in</button>
+			<button class="btn variant-filled-secondary" on:click={() => goto("/signup")}>Sign up</button>
+		</div>
+	</svelte:fragment>
+	<a href="/">
+		<h2 class="h2">Placeholder Name</h2>
+	</a>
+	<svelte:fragment slot="trail">
+		<a href="https://github.com/Theismol/P4-Password-Manager">
+			<Icon icon="mdi:github" height="auto" />
+		</a>
+	</svelte:fragment>
+</AppBar>
  <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
 		<h1 class="h1">Let's get cracking bones!</h1>
