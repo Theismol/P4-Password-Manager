e245becadaa17b9bf09a21436b94bb2959183f45
Nierfeldt <oliver@nierfeldt.dk>
2024-04-01 11:06:15 +0200

    bileld

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import { enhance } from '$app/forms';
-	import MyImage from './logo.png';
+	import MyImage from './Logo.png';
 </script>
 
 <div class="flex h-screen">
@@ -13,11 +13,11 @@
                 </div>
                 <div class="col-span-3" style="color: azure;">
                     <label class="h4" for="username">Username</label>
-                    <input class="input w-full" id="username" type="text" name="username" required />
+                    <input class="input w-full" id="username" type="text" name="username" style="color: black;" required />
                 </div>
                 <div class="col-span-3" style="color: azure;">
                     <label class="h4" for="username">Password</label>
-                    <input class="input w-full" id="password" type="password" name="password" required />
+                    <input class="input w-full" id="password" type="password" name="password" style="color: black;" required />
                 </div>
                 <div class="my-4 col-span-3">
                     <button class="btn variant-filled-primary w-1/6 btn bg-gradient-to-br variant-gradient-tertiary-secondary" type="submit">Log in</button>
