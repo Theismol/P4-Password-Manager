46cc1dee0da64881a8a60dba25a082f0df7b12a3
Theismol <theis.moldrup@gmail.com>
2024-04-20 16:54:45 +0200

    signup done

--- client2/src/services/passwordHash.js
+++ client2/src/services/passwordHash.js
@@ -1,14 +1,10 @@
 var pbkdf2 = require('pbkdf2')
 
-export default function hashPassword(password) {
-    /*var derivedKey = pbkdf2.pbkdf2Sync(password, '', 600000, 256, 'sha512')
-    console.log(derivedKey);*/
-
-    var derivedKey = pbkdf2.pbkdf2('password', '', 600000, 512, 'sha512', (err, derivedKey) => {
-        if (err) throw err;
-        console.log(derivedKey.toString('hex'));
-    
-    })
-
-      return derivedKey;
+export default async function hashPassword(password) {
+    return new Promise((resolve, reject) => {
+        pbkdf2.pbkdf2(password, '', 600000, 512, 'sha512', (err, derivedKey) => {
+            if (err) reject(err);
+            resolve(derivedKey.toString('hex'));
+        });
+    });
 }
