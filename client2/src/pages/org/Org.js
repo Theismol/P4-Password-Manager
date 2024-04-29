3e95a95a8404c484e694145d103b27d40885a749
Nierfeldt <oliver@nierfeldt.dk>
2024-04-29 10:16:16 +0200

    merge bingo

--- client2/src/pages/org/Org.js
+++ client2/src/pages/org/Org.js
@@ -3,9 +3,6 @@ import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import { DataGrid } from "@mui/x-data-grid";
-<<<<<<< HEAD
-import axios from 'axios';
-=======
 import axios from "axios";
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
@@ -13,7 +10,6 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
->>>>>>> orgData
 
 //name, organization id
 
@@ -22,12 +18,7 @@ function Org() {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
   const [selectedRow, setSelectedRow] = useState(null);
-<<<<<<< HEAD
-  const [openModal, setOpenModal] = useState(false);
-  const [csrftoken, setCsrftoken] = useState("");
-=======
   const [open, setOpen] = React.useState(false);
->>>>>>> orgData
   const [rows, setRows] = useState([
     { id: 1, Name: "Jon Snow", email: "Stark" },
     { id: 2, Name: "Cersei Lannister", email: "Lannister" },
@@ -45,42 +36,6 @@ function Org() {
     { field: "Name", headerName: "Name", width: 150 },
   ];
 
-<<<<<<< HEAD
-
-  useEffect(() => {
-    try {
-      axios.get("http://localhost:4000/api/organization/getUserInOrganization", {
-          withCredentials: true,
-      }).then((response) => {
-        const modifiedArray = response.data.users.map(users => {
-          return {...users, id: users._id, Name: users.username, email: users.email  }
-        })
-        setRows(modifiedArray)
-
-
-      }).catch((error) => {
-        console.error('errpr fetching data', error);
-
-      });
-      axios.get("http.//localhost:4000/api/auth/getCSRF").then((response) => {
-        setCsrftoken(response.data.csrftoken);
-      }).catch((error) => {
-        console.log(error);
-      })
-      }
-      catch(error) {
-        console.log(error);
-      }
-      
-    }
-    
-  , []);
-
-
-  function handleSubmit() {
-
-  }
-=======
   const handleClickOpen = (event) => {
     event.preventDefault();
     setOpen(true);
@@ -118,43 +73,27 @@ function Org() {
  
 
   function handleSubmit() {}
->>>>>>> orgData
 
   return (
     <div>
       {console.log(rows)}
       <PermanentDrawerLeft />
       {
-<<<<<<< HEAD
-        <div style={{ display: 'flex', justifyContent: 'center', margin: 'auto' }}>
-=======
         <div
           style={{ display: "flex", justifyContent: "center", margin: "auto" }}
         >
->>>>>>> orgData
           <div style={{ height: 400, width: "40%" }}>
             <DataGrid
               rows={rows}
               columns={columns}
-<<<<<<< HEAD
-              initialState={{
-
-              }}
-
-=======
               initialState={{}}
->>>>>>> orgData
               checkboxSelection
             />
           </div>
 
           <Box
             component="form"
-<<<<<<< HEAD
-            onSubmit={handleSubmit}
-=======
             
->>>>>>> orgData
             noValidate
             sx={{ mt: 1 }}
           >
@@ -167,19 +106,8 @@ function Org() {
             >
               Create org
             </Button>
-<<<<<<< HEAD
-
-          </Box>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
-          >
-=======
           </Box>
           <Box component="form" noValidate sx={{ mt: 1 }} onSubmit={handleClickOpen}>
->>>>>>> orgData
             <Button
               type="submit"
               fullWidth
@@ -187,20 +115,6 @@ function Org() {
               color="primary"
               sx={{ mt: 3, mb: 2 }}
             >
-<<<<<<< HEAD
-              Add/Remove
-            </Button>
-
-          </Box>
-
-
-        </div>
-
-
-
-
-
-=======
               Add
             </Button>
             <Dialog
@@ -244,7 +158,6 @@ function Org() {
             </Dialog>
           </Box>
         </div>
->>>>>>> orgData
       }
     </div>
   );
