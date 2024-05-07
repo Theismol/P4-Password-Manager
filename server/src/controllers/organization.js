eac86e6a6f3e51b860133269da16710a6ca6c5ec
WraasTheOne <nicholas.mazur.hansen@gmail.com>
2024-05-07 15:59:06 +0200

    remove user from org

--- server/src/controllers/organization.js
+++ server/src/controllers/organization.js
@@ -124,5 +124,37 @@ const getUserInOrganization = async (req, res) => {
     }
 };
 
+const removeUserFromOrganization = async (req, res) => {
+    const { organistations, userId } = req.user;
+    const { email } = req.body;
 
-module.exports = { createOrganization, addUserToOrganization, getUserInOrganization };
+    try {
+        const foundOrganization = await organization.findById(organistations);
+        if (!foundOrganization) {
+            return res.status(309).json({ message: 'Organization not found' });
+        }
+        if (!foundOrganization.administrators.includes(userId)) {
+            return res.status(400).json({ message: 'User is not an administrator' });
+        }
+
+        const foundUser = await user.findOne({ email: email });
+        if (!foundUser) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        if (!foundOrganization.users.includes(foundUser._id)) {
+            return res.status(400).json({ message: 'User is not in the organization' });
+        }
+
+        await organization.findOneAndUpdate({ _id: organistations }, { $pull: { users: foundUser._id } });
+        await user.findOneAndUpdate({ _id: foundUser._id }, { organizations: null });
+
+        return res.status(200).json({ message: 'User removed from organization' });
+    } catch (error) {
+        console.error('Error during removing user from organization:', error);
+        return res.status(502).json({ message: 'Internal server error' });
+    }
+}
+
+
+module.exports = { createOrganization, addUserToOrganization, getUserInOrganization, removeUserFromOrganization };
