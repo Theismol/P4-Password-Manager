efdd6c1801f356a8b994d6881c37b7f8aae7e011
Theismol <Theis.moldrup@gmail.com>
2024-05-10 14:23:59 +0200

    password hash is now 256

--- client2/src/services/passwordHash.js
+++ client2/src/services/passwordHash.js
@@ -2,7 +2,7 @@ var pbkdf2 = require('pbkdf2')
 
 export default async function hashPassword(password, iterations) {
     return new Promise((resolve, reject) => {
-        pbkdf2.pbkdf2(password, '', iterations, 512, 'sha512', (err, derivedKey) => {
+        pbkdf2.pbkdf2(password, '', iterations, 256, 'sha256', (err, derivedKey) => {
             if (err) reject(err);
             resolve(derivedKey.toString('hex'));
         });
