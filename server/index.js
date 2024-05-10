2fb808ca8ea115acdc96ea117212c3c6494060fe
Theismol <Theis.moldrup@gmail.com>
2024-05-10 10:01:20 +0200

    CORS origin

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // Allow cross-origin requests
 app.use(cors({
-  origin: true,
+  origin: ["https://accessarmor.server","https://www.accessarmor.server"],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow cookies to be sent with requests
