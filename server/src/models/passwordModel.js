a78eef9ade140ece72b9a20632d5e253705a1576
WraasTheOne <nicholas.mazur.hansen@gmail.com>
2024-05-15 11:14:00 +0200

    slettet organization id from mongo model

--- server/src/models/passwordModel.js
+++ server/src/models/passwordModel.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const passwordSchema = new Schema({
-  organization_id: { type: Schema.Types.ObjectId, ref: 'Organization' },
   user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   username: { type: String, required: true },
